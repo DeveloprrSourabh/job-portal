801d994cf8a4328944f5ab4ca7fa28899c3aa6b6
Sourabh <developrr.sourabh@gmail.com>
2026-06-04 18:21:18 +0530

    Add DELETE endpoint for job resources

--- backend/src/routes/jobRoutes.js
+++ backend/src/routes/jobRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createJob, getAllJobs } from "../controllers/jobController.js";
+import { createJob, deleteJob, getAllJobs, getMyJobs } from "../controllers/jobController.js";
 import protect from "../middleware/authMiddleware.js";
 import recruiterOnly from "../middleware/recruiterMiddleware.js";
 
@@ -10,5 +10,21 @@ router.post("/create", protect, recruiterOnly, createJob);
 
 // Get All Jobs
 router.get("/", getAllJobs);
+
+// Get particular recruiter job
+router.get(
+  "/my-jobs",
+  protect,
+  recruiterOnly,
+  getMyJobs
+);
+
+// Delete Jobs
+router.delete(
+  "/:id",
+  protect,
+  recruiterOnly,
+  deleteJob
+);
 
 export default router;
